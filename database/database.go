862dc0e2cdd94d09e736196d7dad4a47e0478445
Nikolai A. Kuropyatnikov <nickolyan@gmail.com>
2025-06-02 11:03:20 +0300

    Modules

--- database/database.go
+++ database/database.go
@@ -1,26 +1,63 @@
 package database
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
 	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"gopkg.in/yaml.v2"
 )
 
-var DB *sql.DB
+// var pool *pgxpool.Pool
+var DB *pgxpool.Pool
+
+func failOnError(err error, msg string) { //Делаем более читаемую и компактную обработку ошибок.
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
 
 // функция подключения к базе данных
 func Connect() error {
-	connStr := "user=username dbname=catfoodstore sslmode=disable password=yourpassword"
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		return fmt.Errorf("ошибка подключения к БД: %v", err)
+	const configPath = "config.yml"
+	type Cfg struct {
+		POSTGRES_HOST           string `yaml:"postgres_host"`
+		POSTGRES_PORT           int    `yaml:"postgres_port"`
+		POSTGRES_DB             string `yaml:"postgres_db"`
+		POSTGRES_USER           string `yaml:"postgres_user"`
+		POSTGRES_PASS           string `yaml:"postgres_pass"`
+		POSTGRES_SSL            string `yaml:"postgres_ssl"`
+		POSTGRES_POOL_MAX_CONNS int    `yaml:"postgres_pool_max_conns"`
 	}
+	var AppConfig *Cfg
+	f, err := os.Open(configPath)
+	failOnError(err, "Can't open config.\n")
+	defer f.Close()
 
-	if err := db.Ping(); err != nil {
-		return fmt.Errorf("не удалось подключиться к базе данных: %v", err)
-	}
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(&AppConfig)
+	failOnError(err, "Can't decode config.\n")
 
-	DB = db
+	postgres_host := AppConfig.POSTGRES_HOST
+	postgres_port := AppConfig.POSTGRES_PORT
+	postgres_db := AppConfig.POSTGRES_DB
+	postgres_user := AppConfig.POSTGRES_USER
+	postgres_pass := AppConfig.POSTGRES_PASS
+	postgres_ssl := AppConfig.POSTGRES_SSL
+	postgres_pool_max_conns := AppConfig.POSTGRES_POOL_MAX_CONNS
+
+	//Инициализация БД
+	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s&pool_max_conns=%d",
+		postgres_user, postgres_pass, postgres_host, postgres_port, postgres_db, postgres_ssl, postgres_pool_max_conns)
+
+	pool, err := pgxpool.New(context.Background(), dbURL)
+	failOnError(err, "Unable to connection to database: %v.\n")
+	defer pool.Close()
+	log.Print("Connected to database!\n")
+
+	DB = pool
 	log.Println("Успешно подключились к базе данных")
 	return nil
 }
